c6086aa663d0d5aa995482e84037e4720b7c0c58
Achai Broner <achai.broner@gmail.com>
2014-03-26 06:17:16 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ggb::Application.config.secret_key_base = 'ffbd509d466ebcdf09ab23b0819cdbec3576dc148f841c49d68b4588f55306fe55870e4b1e39df3bc8c2fb94b3499f3c44998767fffd211640f4d024f45bc348'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ggb::Application.config.session_store :cookie_store, key: '_ggb_session'
